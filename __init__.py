edd235a17ec4de29195fec877eae936388675bf6
ShayHill <shay_public@hotmail.com>
2019-10-25 22:04:43 -0500

    setup.py tweaks

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from svg_writer import new_svg_root, write_svg, write_png_from_svg
--- setup.py
+++ setup.py
@@ -5,14 +5,17 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='svg_writer',
-    version='0.1',
+    version='0.4',
     author="Shay Hill",
     author_email="shay_public@hotmail.com",
     description="Write SVG files with Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ShayHill/svg_writer",
-    packages=setuptools.find_packages(),
+    py_modules=["svg_writer"],
+    install_requires=['lxml'],
+    tests_require=['pytest'],
+    python_requires='>=3.4',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
