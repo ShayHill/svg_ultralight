8564a1e5acf0a83b4cc35dd15512209374709e47
Shay Hill <shay_public@hotmail.com>
2020-02-24 13:16:23 -0600

    New helpers in new_element. Strip trailing underscores.
    * strip trailing underscores to pass reserved names (e.g., ``in_``)
    * add new parameters to an existing element with ``update_element``
    * create a hard copy of an existing element with ``hardcopy_element``

--- constructors/new_element.py
+++ constructors/new_element.py
@@ -11,6 +11,7 @@ parameters.
 Will translate ``stroke_width=10`` to ``stroke-width="10"``
 """
 
+import copy
 from typing import Union
 
 from lxml import etree  # type: ignore
@@ -21,8 +22,8 @@ def _set_params(elem: etree.Element, **params: Union[str, float]) -> None:
     Set name: value items as element attributes. Make every value a string.
 
     :param elem: element to receive element.set(keyword, str(value)) calls
-    :param params: element attribute names and values ``{'name': 'value'}``. Knows
-        what to do with 'text' keyword.V :effects: updates ``elem``
+    :param params: element attribute names and values. Knows what to do with 'text'
+        keyword.V :effects: updates ``elem``
 
     This is just to save a lot of typing. etree.Elements will only accept string
     values. Takes each in params.values(), and passes it to etree.Element as a
@@ -35,7 +36,7 @@ def _set_params(elem: etree.Element, **params: Union[str, float]) -> None:
     if "text" in params:
         elem.text = params.pop("text")
     for k, v in params.items():
-        elem.set(k.replace("_", "-"), str(v))
+        elem.set(k.rstrip("_").replace("_", "-"), str(v))
 
 
 def new_element(tag: str, **params: Union[str, float]) -> etree.Element:
@@ -43,14 +44,20 @@ def new_element(tag: str, **params: Union[str, float]) -> etree.Element:
     Create an etree.Element, make every kwarg value a string.
 
     :param tag: element tag
-    :param params: element attribute names and values ``{'name': 'value'}``
+    :param params: element attribute names and values
     :returns: new ``tag`` element
 
         >>> elem = new_element('line', x1=0, y1=0, x2=5, y2=5)
         >>> etree.tostring(elem)
         b'<line x1="0" y1="0" x2="5" y2="5"/>'
 
-    Translates underscores to hyphens
+    Strips trailing underscores
+
+        >>> elem = new_element('line', in_="SourceAlpha")
+        >>> etree.tostring(elem)
+        b'<line in="SourceAlpha"/>'
+
+    Translates other underscores to hyphens
 
         >>> elem = new_element('line', stroke_width=1)
         >>> etree.tostring(elem)
@@ -76,7 +83,7 @@ def new_sub_element(
 
     :param parent: parent element
     :param tag: element tag
-    :param params: element attribute names and values ``{'name': 'value'}``
+    :param params: element attribute names and values
     :returns: new ``tag`` element
 
         >>> parent = etree.Element('g')
@@ -86,6 +93,31 @@ def new_sub_element(
     """
     elem = etree.SubElement(parent, tag)
     _set_params(elem, **params)
+    return elem
+
+
+def update_element(elem: etree.Element, **params: Union[str, float]) -> etree.Element:
+    """
+    Update an existing etree.Element with additional params.
+
+    :param elem: at etree element
+    :param params: element attribute names and values
+    """
+    _set_params(elem, **params)
+    return elem
+
+
+def deepcopy_element(elem: etree.Element, **params: Union[str, float]) -> etree.Element:
+    """
+    Create a deepcopy of an element. Optionally pass additional params.
+
+    :param elem: at etree element
+    :param params: element attribute names and values
+    """
+    if isinstance(elem, list):
+        return [deepcopy_element(x, **params) for x in elem]
+    elem = copy.deepcopy(elem)
+    update_element(elem, **params)
     return elem
 
 
--- test/test_new_element.py
+++ test/test_new_element.py
@@ -6,7 +6,7 @@
 :created: 1/31/2020
 """
 
-from new_element import new_element, new_sub_element
+from new_element import new_element, new_sub_element, update_element, deepcopy_element
 from lxml import etree  # type: ignore
 
 
@@ -15,6 +15,11 @@ class TestNewElement:
         """Replace _ with -. Pass params.values() as strings"""
         elem = new_element("line", x=10, y1=80, stroke_width="2")
         assert etree.tostring(elem) == b'<line x="10" y1="80" stroke-width="2"/>'
+
+    def test_trailing_underscore(self) -> None:
+        """Remove trailing _ from params"""
+        elem = new_element("line", x=10, y1=80, in_="SourceAlpha")
+        assert etree.tostring(elem) == b'<line x="10" y1="80" in="SourceAlpha"/>'
 
     def test_param_text(self) -> None:
         """Insert text between tags with parameters"""
@@ -35,5 +40,23 @@ class TestNewSubElement:
         assert etree.tostring(parent) == b"<g><rect/></g>"
 
 
+class TestUpdateElement:
+    def test_new_params(self) -> None:
+        """New params added"""
+        elem = new_element("line", x=10, y1=80)
+        update_element(elem, stroke_width=2)
+        assert etree.tostring(elem) == b'<line x="10" y1="80" stroke-width="2"/>'
 
 
+class TestDeepcopyElement:
+    def test_is_copy(self) -> None:
+        """"""
+        group = new_element("g")
+        line = new_sub_element(group, "line", x=10, y1=80)
+        group_copy = deepcopy_element(group, stroke="black")
+        assert (
+            etree.tostring(group_copy)
+            == b'<g stroke="black"><line x="10" y1="80"/></g>'
+        )
+        assert line in group
+        assert line not in group_copy
