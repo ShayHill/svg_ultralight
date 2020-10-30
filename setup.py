75ca6dc222208fedf60a16bed989812ddd5eb8ec
Shay Hill <shay_public@hotmail.com>
2020-10-30 16:17:21 -0500

    corrected import error in __init__.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name="svg_ultralight",
-    version="0.5.1",
+    version="0.5.2",
     author="Shay Hill",
     author_email="shay_public@hotmail.com",
     description="Write SVG files with Python.",
