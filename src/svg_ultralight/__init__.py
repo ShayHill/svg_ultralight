a9d9d54933bdcead9a21060dabdc8eb9849dfd65
Shay Hill <shay_public@hotmail.com>
2022-12-09 22:36:36 -0600

    most functions in top-level namespace
    * previous svg_ultralight.constructor and svg_ultralight.query functions
      now available in the top-level namespace.
    * Previous BoundingBox class and new bounding-box cousins BoundElem and
      PaddedText are also available from the top-level namespace.

--- src/svg_ultralight/__init__.py
+++ src/svg_ultralight/__init__.py
@@ -3,4 +3,8 @@
 :author: Shay Hill
 created: 12/22/2019
 """
+from svg_ultralight.bounding_boxes.type_bound_element import BoundElement
+from svg_ultralight.bounding_boxes.type_bounding_box import BoundingBox
+from svg_ultralight.bounding_boxes.type_padded_text import PaddedText
+from svg_ultralight.constructors.new_element import *
 from svg_ultralight.main import *
