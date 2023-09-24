fbdbadeb9ca156c82d4b59784f4ec8638a0565da
Shay Hill <shay_public@hotmail.com>
2023-09-23 22:08:42 -0500

    feat: format attr dicts with project fixes
    Create an attribute dictionary with format_attr_dict. Keys and values in the dictionary will be processed through the usual svg_ultralight fixers. E.g., '-' becomes '_', float values are formatted to strings.

--- src/svg_ultralight/string_conversion.py
+++ src/svg_ultralight/string_conversion.py
@@ -48,12 +48,12 @@ def format_numbers(nums: Iterable[float]) -> list[str]:
     return [format_number(num) for num in nums]
 
 
-def set_attributes(elem: EtreeElement, **attributes: str | float) -> None:
-    """Set name: value items as element attributes. Make every value a string.
+def _fix_key(key: str, val: str | float) -> tuple[str, str]:
+    """Format one key, value pair for an svg element.
 
-    :param elem: element to receive element.set(keyword, str(value)) calls
-    :param attributes: element attribute names and values. Knows what to do with 'text'
-        keyword.V :effects: updates ``elem``
+    :param key: element attribute name
+    :param val: element attribute value
+    :return: tuple of key, value
 
     This is just to save a lot of typing. etree.Elements will only accept string
     values. Takes each in params.values(), and passes it to etree.Element as a
@@ -69,22 +69,42 @@ def set_attributes(elem: EtreeElement, **attributes: str | float) -> None:
     Format floats through f'{value:f} to avoid exponential representation
     (e.g., 10e-06) which svg parsers won't understand.
     """
+    if ":" in key:
+        namespace, tag = key.split(":")
+        key_ = str(etree.QName(NSMAP[namespace], tag))
+    else:
+        key_ = key.rstrip("_").replace("_", "-")
+    try:
+        val_ = format_number(float(val))
+    except ValueError:
+        val_ = str(val)
+    return key_, val_
+
+
+def format_attr_dict(**attributes: str | float) -> dict[str, str]:
+    """Use svg_ultralight key / value fixer to create a dict of attributes.
+
+    :param attributes: element attribute names and values. Knows what to do with 'text'
+    :return: dict of attributes, each key a valid svg attribute name, each value a str
+    """
+    return dict(_fix_key(key, val) for key, val in attributes.items())
+
+
+def set_attributes(elem: EtreeElement, **attributes: str | float) -> None:
+    """Set name: value items as element attributes. Make every value a string.
+
+    :param elem: element to receive element.set(keyword, str(value)) calls
+    :param attributes: element attribute names and values. Knows what to do with 'text'
+        keyword.V :effects: updates ``elem``
+    """
+    attr_dict = format_attr_dict(**attributes)
+
     dots = {"text"}
-    for dot in dots & set(attributes):
-        setattr(elem, dot, attributes.pop(dot))
+    for dot in dots & set(attr_dict):
+        setattr(elem, dot, attr_dict.pop(dot))
 
-    for key, val in attributes.items():
-        if ":" in key:
-            namespace, tag = key.split(":")
-            key_ = etree.QName(NSMAP[namespace], tag)
-        else:
-            key_ = key.rstrip("_").replace("_", "-")
-
-        try:
-            val_as_str = format_number(float(val))
-        except ValueError:
-            val_as_str = str(val)
-        elem.set(key_, val_as_str)
+    for key, val in attr_dict.items():
+        elem.set(key, val)
 
 
 class _TostringDefaults(Enum):
