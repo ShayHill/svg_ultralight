fb1f43010044ee4eb22b64d44e20ee8f57d8fbc2
Shay Hill <shay_public@hotmail.com>
2024-03-19 21:05:01 -0500

    feat: format datastrings
    Will format datastring values for `d` keyword arguments. Correctly identifies and reformats exponential floats and float strings.

--- src/svg_ultralight/string_conversion.py
+++ src/svg_ultralight/string_conversion.py
@@ -10,6 +10,7 @@ Rounding some numbers to ensure quality svg rendering:
 
 from __future__ import annotations
 
+import re
 from enum import Enum
 from typing import TYPE_CHECKING, cast
 
@@ -23,7 +24,7 @@ if TYPE_CHECKING:
     from lxml.etree import _Element as EtreeElement  # type: ignore
 
 
-def format_number(num: float) -> str:
+def format_number(num: float | str) -> str:
     """Format strings at limited precision.
 
     :param num: anything that can print as a float.
@@ -33,14 +34,21 @@ def format_number(num: float) -> str:
     after the decimal point to ensure good svg rendering. I'm being generous and
     giving six. Mostly to eliminate exponential notation, but I'm "rstripping" the
     strings to reduce filesize and increase readability
+
+    * reduce fp precision to 6 digits
+    * remove trailing zeros
+    * remove trailing decimal point
+    * convert "-0" to "0"
     """
-    as_str = f"{num:0.6f}".rstrip("0").rstrip(".")
+    as_str = f"{float(num):0.6f}".rstrip("0").rstrip(".")
     if as_str == "-0":
         as_str = "0"
     return as_str
 
 
-def format_numbers(nums: Iterable[float]) -> list[str]:
+def format_numbers(
+    nums: Iterable[float] | Iterable[str] | Iterable[float | str],
+) -> list[str]:
     """Format multiple strings to limited precision.
 
     :param nums: iterable of floats
@@ -49,46 +57,81 @@ def format_numbers(nums: Iterable[float]) -> list[str]:
     return [format_number(num) for num in nums]
 
 
-def _fix_key(key: str, val: str | float) -> tuple[str, str]:
+def _is_float_or_float_str(data: float | str) -> bool:
+    """Check if a string is a float.
+
+    :param data: string to check
+    :return: bool
+    """
+    try:
+        _ = float(data)
+    except ValueError:
+        return False
+    else:
+        return True
+
+
+def _format_datastring(data: str) -> str:
+    """Find and format floats in a string.
+
+    :param data: string with floats
+    :return: string with floats formatted to limited precision
+
+    Will correctly handle input floats in exponential notation.
+    """
+    words = re.split(r"([^\d.eE-]+)", data)
+    words = [format_number(w) if _is_float_or_float_str(w) else w for w in words]
+    return "".join(words)
+
+
+def _fix_key_and_format_val(key: str, val: str | float) -> tuple[str, str]:
     """Format one key, value pair for an svg element.
 
     :param key: element attribute name
     :param val: element attribute value
     :return: tuple of key, value
+    :raises ValueError: if key is 'd' and val is not a string
 
-    This is just to save a lot of typing. etree.Elements will only accept string
-    values. Takes each in params.values(), and passes it to etree.Element as a
-    string. Will also replace `_` with `-` to translate valid Python variable names
-    for xml parameter names.
+    etree.Elements will only accept string
+    values. This saves having to convert input to strings.
 
-    Invalid names (a popular one will be 'class') can be passed with a trailing
-    underscore (e.g., class_='body_text').
+    * convert float values to formatted strings
+    * format datastring values when keyword is 'd'
+    * replace '_' with '-' in keywords
+    * remove trailing '_' from keywords
+    * will convert `namespace:tag` to a qualified name
 
-    That's almost all. The function will also handle the 'text' keyword, placing the
-    value between element tags.
+    SVG attribute names like `font-size` and `stroke-width` are not valid python
+    keywords, but can be passed as `font-size` and `stroke-width`.
 
-    Format floats through f'{value:f} to avoid exponential representation
-    (e.g., 10e-06) which svg parsers won't understand.
+    Reserved Python keywords that are also valid and useful SVG attribute names (a
+    popular one will be 'class') can be passed with a trailing underscore (e.g.,
+    class_='body_text').
     """
     if ":" in key:
         namespace, tag = key.split(":")
         key_ = str(etree.QName(NSMAP[namespace], tag))
     else:
         key_ = key.rstrip("_").replace("_", "-")
-    try:
-        val_ = format_number(float(val))
-    except ValueError:
-        val_ = str(val)
-    return key_, val_
+
+    if key_ == "d":
+        if isinstance(val, str):
+            return key_, _format_datastring(val)
+        msg = f"Expected string for 'd' attribute, got {val} of type {type(val)}"
+        raise ValueError(msg)
+
+    if _is_float_or_float_str(val) and key_ != "text":
+        return key_, format_number(val)
+    return key_, str(val)
 
 
 def format_attr_dict(**attributes: str | float) -> dict[str, str]:
     """Use svg_ultralight key / value fixer to create a dict of attributes.
 
-    :param attributes: element attribute names and values. Knows what to do with 'text'
+    :param attributes: element attribute names and values.
     :return: dict of attributes, each key a valid svg attribute name, each value a str
     """
-    return dict(_fix_key(key, val) for key, val in attributes.items())
+    return dict(_fix_key_and_format_val(key, val) for key, val in attributes.items())
 
 
 def set_attributes(elem: EtreeElement, **attributes: str | float) -> None:
--- tests/test_string_conversion.py
+++ tests/test_string_conversion.py
@@ -4,12 +4,16 @@
 :created: 2023-09-23
 """
 
+import pytest
+
 import svg_ultralight.string_conversion as mod
+
 
 class TestFormatNuber:
     def test_negative_zero(self):
         """Remove "-" from "-0"."""
         assert mod.format_number(-0.0000000001) == "0"
+
     def test_round_to_int(self):
         """Round to int if no decimal values !- 0."""
         assert mod.format_number(1.0000000001) == "1"
@@ -23,3 +27,45 @@ class TestFormatNumbers:
     def test_explicit(self):
         """Return list of formatted strings."""
         assert mod.format_numbers([1, 2, 3]) == ["1", "2", "3"]
+
+
+class TestFormatAttrDict:
+    def test_float(self):
+        """Return string of float."""
+        assert mod.format_attr_dict(x=1.0) == {"x": "1"}
+
+    def test_float_string(self):
+        """Return string of float."""
+        assert mod.format_attr_dict(x="1.0") == {"x": "1"}
+
+    def test_exponential_float(self):
+        """Return string of float."""
+        assert mod.format_attr_dict(x="1.0e-10") == {"x": "0"}
+
+    def test_exponential_float_string(self):
+        """Return string of float."""
+        assert mod.format_attr_dict(x=1.0e-10) == {"x": "0"}
+
+    def test_datastring(self):
+        """Find and format floats in a datastring."""
+        assert mod.format_attr_dict(d="M1.0,0 Q -0,.33333333 1,2z") == {
+            "d": "M1,0 Q 0,0.333333 1,2z"
+        }
+
+    def test_datastring_with_exponential_number(self):
+        """Find and format floats in a datastring."""
+        assert mod.format_attr_dict(d="M1.0,1.0e-10 Q -0,.33333333 1,2z") == {
+            "d": "M1,0 Q 0,0.333333 1,2z"
+        }
+
+    def test_invalid_datastring(self):
+        """Raise ValueError if d value is not a string."""
+        assert pytest.raises(ValueError, mod.format_attr_dict, d=1)
+
+    def test_trailing_underscore(self):
+        """Remove trailing underscore from key."""
+        assert mod.format_attr_dict(x_=1) == {"x": "1"}
+
+    def test_replace_underscore(self):
+        """Replace underscore with hyphen."""
+        assert mod.format_attr_dict(x_y=1) == {"x-y": "1"}
