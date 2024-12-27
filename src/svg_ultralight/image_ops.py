b3cccfdb3fa06f72737640c08887548ffdb19d65
Shay <shay_public@hotmail.com>
2024-12-26 23:30:26 -0600

    chore: update pre-commit hooks

--- src/svg_ultralight/image_ops.py
+++ src/svg_ultralight/image_ops.py
@@ -31,9 +31,8 @@ import io
 from lxml import etree
 
 from svg_ultralight import NSMAP
-from svg_ultralight.bounding_boxes.type_bounding_box import BoundingBox
-from svg_ultralight.constructors import new_element
 from svg_ultralight.bounding_boxes.bound_helpers import bbox_dict
+from svg_ultralight.constructors import new_element
 
 if TYPE_CHECKING:
     from pathlib import Path
@@ -41,6 +40,8 @@ if TYPE_CHECKING:
     from lxml.etree import (
         _Element as EtreeElement,  # pyright: ignore [reportPrivateUsage]
     )
+
+    from svg_ultralight.bounding_boxes.type_bounding_box import BoundingBox
 
 
 def _symmetric_crop(
