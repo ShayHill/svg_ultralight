f023d4a4c3a3426062627b41bdc4193e8fb0de4b
Shay Hill <shay_public@hotmail.com>
2025-12-19 12:01:39 -0600

    fix: import PaddedList into package namespace

--- src/svg_ultralight/__init__.py
+++ src/svg_ultralight/__init__.py
@@ -25,6 +25,7 @@ from svg_ultralight.bounding_boxes.supports_bounds import SupportsBounds
 from svg_ultralight.bounding_boxes.type_bound_collection import BoundCollection
 from svg_ultralight.bounding_boxes.type_bound_element import BoundElement
 from svg_ultralight.bounding_boxes.type_bounding_box import BoundingBox
+from svg_ultralight.bounding_boxes.type_padded_list import PaddedList
 from svg_ultralight.bounding_boxes.type_padded_text import PaddedText, new_padded_union
 from svg_ultralight.constructors.new_element import (
     deepcopy_element,
@@ -66,6 +67,7 @@ __all__ = [
     "BoundCollection",
     "BoundElement",
     "BoundingBox",
+    "PaddedList",
     "PaddedText",
     "SupportsBounds",
     "align_tspans",
